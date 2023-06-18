d3dddec26273c18a18b335fc98a960a4b10bf351
LinCY0202 <136959185+LinCY0202@users.noreply.github.com>
2023-06-18 17:04:01 +0800

    Delete logic_design directory

--- logic_design/homework1/BCD2Excess6.v
+++
@@ -1,9 +0,0 @@
-module BCD2Excess6(W,X,Y,Z,A,B,C,D);
-  output W,X,Y,Z;
-  input	A,B,C,D;
-
-  assign Z=D,
-  Y=!C,
-  X=((!B)&&(!C))||(B&&C),
-  W=A || B || C;
-endmodule
--- logic_design/homework4/problem4/problem4.v
+++
@@ -1,8 +0,0 @@
-module problem4(SO,SI,clk);
-    output SO;
-    input SI,clk;
-    reg [3:0]Q; 
-    assign SO=Q[0];
-    always@(posedge clk)
-        Q={SI,Q[3:1]};
-endmodule
